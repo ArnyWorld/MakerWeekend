73949140649892ba9caab445de5b39ffb47b9ed4
Arnaldo Muñoz Mendoza <47994871+ArnyWorld@users.noreply.github.com>
2019-07-05 10:08:44 -0400

    subiendo taller nodemcu y blynk

---
+++ tallerNodemcyuBlynk/dhtBlynk/dhtBlynk.ino
@@ -0,0 +1,40 @@
+#define BLYNK_PRINT Serial
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+#include <ESP8266WiFi.h>
+#include <DHT.h>
+#define DHTPIN D1 //pin donde conectamos el sensor
+#define DHTTYPE DHT22 // iniciamos el dht11
+DHT dht(DHTPIN, DHTTYPE);
+
+const char auth[]="COPIAR TOKEN DEL EMAIL";
+const char* ssid ="NOMBRE DE LA RED WIFI";//red WiFi
+const char* pass="CONTRASEÑA DE LA RED WIFI";//contraseña de la red WiFi
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  dht.begin();
+  Blynk.begin(auth,ssid,pass);
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+ // lectura del sensor de temperatura y humedad
+ float t=dht.readTemperature();
+ float h=dht.readHumidity();
+
+ Blynk.virtualWrite(V1,t);
+ Blynk.virtualWrite(V2,h);
+
+ Serial.println("Temperatura : ");
+ Serial.println(t);
+  Serial.println("Humedad: ");
+ Serial.println(h);
+
+ 
+}
+
+
+
---
+++ tallerNodemcyuBlynk/servoBlynk/servoBlynk.ino
@@ -0,0 +1,31 @@
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+#include <Servo.h>
+
+Servo servo;
+
+const char auth[]="COPIAR TOKEN EMAIL";
+const char* ssid ="NOMBRE RED WIFI";//red WiFi
+const char* pass="CONTRASEÑA RED WIFI";//contraseña de la red WiFi
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Blynk.begin(auth,ssid,pass);
+  servo.attach(D2);
+  
+}
+void loop() {
+ Blynk.run();
+}
+
+BLYNK_WRITE(V3){
+  servo.write(param.asInt());
+}
+
+
+
+
+
+
+
